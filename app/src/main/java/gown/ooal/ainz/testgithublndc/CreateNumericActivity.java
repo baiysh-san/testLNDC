1263d4ec9f6a41daba6254784a90d4c039f36284
bat17i12 <Baiysh.Toktosunov.i12@fh-zwickau.de>
2018-11-09 18:50:55 +0100

    bai3

--- app/src/main/java/gown/ooal/ainz/testgithublndc/CreateNumericActivity.java
+++ app/src/main/java/gown/ooal/ainz/testgithublndc/CreateNumericActivity.java
@@ -1,13 +1,41 @@
 package gown.ooal.ainz.testgithublndc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateNumericActivity extends AppCompatActivity {
-
+    private Intent intentMain;
+    private EditText nameEditText;
+    private EditText sidesNumber;
+    private Button saveButton;
+    private Button removeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_numeric);
+        intentMain = new Intent(this, MainActivity.class);
+        nameEditText = findViewById(R.id.editText_cn_name);
+        sidesNumber = findViewById(R.id.editText_cn_sides);
+        saveButton = findViewById(R.id.btn_cn_save);
+        removeButton = findViewById(R.id.btn_cn_remove);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intentMain);
+            }
+        });
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentMain);
+            }
+        });
+
     }
 }
--- app/src/main/java/gown/ooal/ainz/testgithublndc/TypeActivity.java
+++ app/src/main/java/gown/ooal/ainz/testgithublndc/TypeActivity.java
@@ -3,13 +3,24 @@ package gown.ooal.ainz.testgithublndc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TypeActivity extends AppCompatActivity {
-
+    private Button numericButton;
+    private Intent createNumericIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
         setContentView(R.layout.activity_type);
+        numericButton = findViewById(R.id.button_t_numeric);
+        createNumericIntent = new Intent(this, CreateNumericActivity.class);
+        numericButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(createNumericIntent);
+            }
+        });
+
     }
 }
