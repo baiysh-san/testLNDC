92000bfad30b4da630f79013f02126755a71ecaa
zht17i11 <Zhanysh.Toktosunov.i11@fh-zwickau.de>
2018-11-09 16:23:44 +0100

    Add Activities

--- app/src/main/java/gown/ooal/ainz/testgithublndc/MainActivity.java
+++ app/src/main/java/gown/ooal/ainz/testgithublndc/MainActivity.java
@@ -2,6 +2,8 @@ package gown.ooal.ainz.testgithublndc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_create = findViewById(R.id.button_m_create);
+        button_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivityForResult();
+                setContentView(R.layout.activity_type);
+            }
+        });
     }
 }
